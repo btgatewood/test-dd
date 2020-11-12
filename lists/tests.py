0e5d3e77714919d05b8ae8fcdf41f4929dabfa43
Bradley Gatewood <gatewoodbt@outlook.com>
2020-11-12 13:38:56 -0500

    Home view returns minimal HTML

--- lists/tests.py
+++ lists/tests.py
@@ -2,10 +2,20 @@ from django.test import TestCase
 
 # Create your tests here.
 
+from django.http import HttpRequest
 from django.urls import resolve
+
 from lists.views import home
+
 
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home)
+    
+    def test_home_page_returns_correct_html(self):
+        response = home(HttpRequest())
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do Lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
--- lists/views.py
+++ lists/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home():
-    pass
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('<html><title>To-Do Lists</title></html>')

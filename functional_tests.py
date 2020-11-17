dca7a1a71255e9b884c9cfaf4e175d1c15090ab3
Bradley Gatewood <gatewoodbt@outlook.com>
2020-11-17 12:34:36 -0500

    Add input and table to home template

--- functional_tests.py
+++ functional_tests.py
@@ -35,7 +35,8 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: buy groceries' for row in rows))
+        self.assertTrue(any(row.text == '1: buy groceries' for row in rows),
+                            'New to-do item did not appear in table')
 
         # A text box prompts the user to add another task.  User enters "cook
         # dinner".

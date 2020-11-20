8864395b3cfd392364e3d735448c6a80deace142
Bradley Gatewood <gatewoodbt@outlook.com>
2020-11-19 19:59:54 -0500

    Add model for list items, make initial migrations

--- lists/tests.py
+++ lists/tests.py
@@ -2,10 +2,27 @@ from django.test import TestCase
 
 # Create your tests here.
 
+from lists.models import Item
 from lists.views import home
 
 
-class HomePageTest(TestCase):    
+class ItemModelTest(TestCase):
+    def test_saving_items(self):
+        item1 = Item()
+        item1.text = 'the first item'
+        item1.save()
+
+        item2 = Item()
+        item2.text = 'a second item'
+        item2.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+        self.assertEqual(item1.text, saved_items[0].text)
+        self.assertEqual(item2.text, saved_items[1].text)
+
+
+class HomeViewTest(TestCase):    
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
